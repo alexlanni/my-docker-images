52005db4daeac141daea26bdeadde1eeb822ee8e
vagrant <vagrant@vagrant.vm>
2021-04-08 10:44:28 +0000

    fix: remove comment from push command

--- rebuild.sh
+++ rebuild.sh
@@ -12,6 +12,6 @@ if [ "$1" = "php7.4-apache-custom" ] || [ "$1" = "all" ]
 then
 	echo "Rebuild php7.4-apache-custom"
 	docker build --tag=alessandrolanni/php7.4-apache-custom ./php/7.4-apache-custom
-	#docker push alessandrolanni/php7.4-apache-custom
+	docker push alessandrolanni/php7.4-apache-custom
 fi
 
