4922f66ccf88f99c118b70f3c87ee4b89982c664
Alessandro Lanni <alessandrolanni@MacBook-Pro-di-Alessandro.local>
2022-07-09 11:49:50 +0200

    Fix rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -32,6 +32,5 @@ fi
 if [ "$1" = "php8.1-apache-custom" ] || [ "$1" = "all" ]
 then
         echo "Rebuild php8.1-apache-custom"
-        docker build --platform linux/amd64,linux/arm64 --tag=alessandrolanni/php8.1-apache-custom ./php/8.1-apache-custom
-        docker push alessandrolanni/php8.1-apache-custom
+        docker buildx build --platform linux/amd64,linux/arm64 --tag=alessandrolanni/php8.1-apache-custom --push ./php/8.1-apache-custom
 fi
